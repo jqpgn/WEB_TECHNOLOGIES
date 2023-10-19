fd27309315ac7fb906fc511d77df86c0f32eed5a
jqpgn <144214883+jqpgn@users.noreply.github.com>
2023-10-19 20:45:25 +0300

    Add files via upload

--- education/typography/algorithm/algorithm.js
+++ education/typography/algorithm/algorithm.js
@@ -6,40 +6,32 @@ function verify() {
     console.log(a, b, c)
 
     if ((a + b > c && a + c > b && b + c > a) && ((Math.abs(a*a + b*b - c*c) < 0.2) || (Math.abs(a*a + c*c - b*b) < 0.2) || (Math.abs(c*c + b*b - a*a) < 0.2))) {
-        result = "Треугольник с такими сторонами существует,\nи он прямоугольный!";
+        result = "Треугольник с такими сторонами существует,\n и он прямоугольный!";
         document.getElementById("result").innerText = messageText + result;
-        document.getElementById("result").value = result;
+        document.getElementById("result2").value = result;
         check = true;
     }
     else if ((isNaN(a)) || (isNaN(b)) || (isNaN(c))) {
         document.getElementById("result").innerText = messageText + 'Произошла ошибка. Повторите ввод!';
+        document.getElementById("result2").value = 'Произошла ошибка. Повторите ввод!';
     }
     else if (!(a + b > c && a + c > b && b + c > a)) {
         result = "Треугольник с такими сторонами НЕ существует!";
         document.getElementById("result").innerText = messageText + result;
-        document.getElementById("result").value = result;
+        document.getElementById("result2").value = result;
         check = false;
     }
     else {
-        document.getElementById("result").innerText = messageText + "Треугольник с такими сторонами существует,\nно он НЕ прямоугольный!";
+        document.getElementById("result").innerText = messageText + "Треугольник с такими сторонами существует,\n но он НЕ прямоугольный!";
+        document.getElementById("result2").value = "Треугольник с такими сторонами существует,\n но он НЕ прямоугольный!";
     }
 }
 
 
-function send() {
-    if (check) {
-        let textCondition = document.getElementsByTagName('p')[0].innerText
-        document.document.getElementsByTagName('p')[0].value = textCondition;
-        document.getElementsByName('result')[0].value = result;
-        document.getElementById("form").submit();
-    } else {
-        document.getElementById("result").innerText = messageText + 'Произошла ошибка. Повторите ввод';
-    }
-}
+
 
 function verify_send() {
     verify();
-    send();
 }
 
 
@@ -58,5 +50,5 @@ elementB.addEventListener('keyup', verify);
 const elementC = document.getElementById("c");
 elementC.addEventListener('keyup', verify);
 
-const elementSend = document.getElementById("send");
-elementSend.addEventListener('click', send)
+
+
